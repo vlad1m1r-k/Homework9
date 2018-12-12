0a2e68db6a9166326fc4f4444142b052af570f08
Vladimir <vova.kvz3@gmail.com>
2018-12-12 11:49:46 +0200

    ""

--- src/ua/kiev/prog/homework9/part3/Main.java
+++ src/ua/kiev/prog/homework9/part3/Main.java
@@ -6,37 +6,49 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
+    private static Map<Character, Float> data = new HashMap<>(26);
+
     public static void main(String[] args) {
-        Map<Character, Integer> data = new HashMap<>(26);
+        parseFile();
+        int valuesSum = 0;
+        for (Float value : data.values()) {
+            valuesSum += value;
+        }
+        for (Map.Entry<Character, Float> entry : data.entrySet()) {
+            data.replace(entry.getKey(), (entry.getValue() / valuesSum) * 100);
+        }
+        Set<Map.Entry<Character, Float>> set = new TreeSet<>((o1, o2) -> comparator(o1, o2));
+        set.addAll(data.entrySet());
+        for (Map.Entry<Character, Float> entry : set){
+            System.out.printf("%c = %.2f %%\n", entry.getKey(), entry.getValue());
+        }
+    }
+
+    private static int comparator(Map.Entry<Character, Float> entry1, Map.Entry<Character, Float> entry2) {
+        float result = entry1.getValue() - entry2.getValue();
+        if (result == 0) return 0;
+        return result > 0 ? -1 : 1;
+    }
+
+    private static void parseFile() {
         try {
             List<String> strings = Files.readAllLines(Paths.get("text.txt"));
             for (String string : strings) {
                 for (int i = 0; i < string.length(); i++) {
                     String character = string.toLowerCase().substring(i, i + 1);
                     if (character.matches("[a-z]")) {
-                        if (data.containsKey(character.charAt(0))){
-                            int value = data.get(character.charAt(0));
+                        if (data.containsKey(character.charAt(0))) {
+                            float value = data.get(character.charAt(0));
                             value++;
                             data.replace(character.charAt(0), value);
                         } else {
-                            data.put(character.charAt(0), 1);
+                            data.put(character.charAt(0), 1f);
                         }
                     }
                 }
-
             }
-            System.out.println(data);
-            int valuesSum = 0;
-            for (Integer value : data.values()){
-                valuesSum += value;
-            }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static int comparator(Integer set1, Integer set2){
-        return set1 - set2;
     }
 }
